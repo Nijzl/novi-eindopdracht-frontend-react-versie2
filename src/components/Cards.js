cdadd3d78981e3f131e479479d74886efa196173
Nijzl <91056065+Nijzl@users.noreply.github.com>
2022-12-09 11:36:07 +0100

    Github test

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,8 +10,6 @@ function Cards(){
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem src="../assets/food2.png" text=" This is text " label=" This is label " path="/favourites"/> {/* ITEM MOET AANGEPAST WORDEN AAN API */}
-                        <CardItem src="../assets/food2.png" text=" This is text " label=" This is label " path="/favourites"/> {/* ITEM MOET AANGEPAST WORDEN AAN API */}
-                        <CardItem src="../assets/food2.png" text=" This is text " label=" This is label " path="/favourites"/> {/* ITEM MOET AANGEPAST WORDEN AAN API */}
                     </ul>
                 </div>
             </div>
